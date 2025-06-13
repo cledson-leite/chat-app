c6290451cc2827aa525ea4b3c7c169b52cf41759
cledson-leite <csbetsonline@gmail.com>
2025-06-13 13:41:00 -0300

    prodected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,40 @@
 import { useEffect } from 'react'
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route, Navigate} from 'react-router-dom'
+import { LuLoaderCircle } from "react-icons/lu"
+
+import { useAuthStore } from './stores/useAuthStore'
+
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
-import { useAuthStore } from './stores/useAuthStore'
 
 
 export default function App() {
-  const {authUser, checkAuth} = useAuthStore()
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
 
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
+  
 
-  console.log(authUser)
-  return (
-    <div>
-      <Navbar />
-      <Routes >
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/settings' element={<SettingsPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
-      </Routes>
-  </div>
-  )
+  return isCheckingAuth && !authUser 
+    ? (
+        <div className='flex justify-center items-center h-screen'>
+          <LuLoaderCircle className="size-10 animate-spin" />
+        </div>
+      )
+    : (<div>
+          <Navbar />
+          <Routes >
+            <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+            <Route path='/signup' element={!authUser ? <SignupPage /> : <Navigate to='/' />} />
+            <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+            <Route path='/settings' element={<SettingsPage />} />
+            <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+          </Routes>
+        </div>
+      )
 }
